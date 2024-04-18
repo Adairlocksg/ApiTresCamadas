8081abbd1cd157544dd10ce71459d6734fbe1fef
adair.grassi <adair.grassi@useall.com.bt>
2024-04-18 14:00:35 -0300

    feat/criando camada de negócios

--- src/DevIO.Domain/Models/Fornecedor.cs
+++ src/DevIO.Domain/Models/Fornecedor.cs
@@ -1,4 +1,6 @@
-﻿namespace DevIO.Business.Models
+﻿using System.Collections.Generic;
+
+namespace DevIO.Business.Models
 {
     public class Fornecedor : Entity
     {
@@ -7,5 +9,7 @@
         public TipoFornecedor TipoFornecedor { get; set; }
         public bool Ativo { get; set; }
         public Endereco Endereco { get; set; }
+
+        public IEnumerable<Produto> Produtos { get; set; }
     }
 }
